c48b2a4a4ba06589ba1c58faf98b11ee6f39d316
astefanacci15 <astefanacci15@sccsc.edu>
2024-04-07 15:36:30 -0400

    Uploading Project
    Uploading project files.

---
+++ StudentProfileWebApi/ConsumeWebAPI/Controllers/APIController.cs
@@ -0,0 +1,31 @@
+﻿using ConsumeWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ConsumeWebAPI.Controllers
+{
+    public class APIController : Controller
+    {
+        Uri baseAddress = new Uri("https://localhost:44361/api");
+        private readonly HttpClient _httpClient;
+        public APIController()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = baseAddress;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<ClassModel> classes = new List<ClassModel>();
+            HttpResponseMessage response = _httpClient.GetAsync(_httpClient.BaseAddress).Result;
+
+            if(response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                classes = JsonConvert.DeserializeObject<List<ClassModel>>(data);
+            }
+            return View(classes);
+        }
+    }
+}
---
+++ StudentProfileWebApi/ConsumeWebAPI/Models/ClassModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsumeWebAPI.Models
+{
+    public class ClassModel
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public double GPA { get; set; }
+        public string? StudentEmail { get; set; }
+        public string? Major { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? StudentUserName { get; set; }
+        public string? password { get; set; }
+        public string? hashedPassword { get; set; }
+        public Boolean isVerified { get; set; }
+    }
+}
